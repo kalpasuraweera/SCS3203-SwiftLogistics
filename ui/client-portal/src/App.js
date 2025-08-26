18fb6cd72ad4feb1084cd2bf9db41d820f100f07
kalpa madhushan <kalpamadhushan18@gmail.com>
2025-08-26 12:48:16 +0530

    Initialize client and driver applications with basic structure and dependencies

---
+++ ui/client-portal/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Client Portal</h1>
+      <p>Welcome to the SwiftTrack Client Portal!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ ui/driver-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Driver App</h1>
+      <p>Welcome to the SwiftTrack Driver App!</p>
+    </div>
+  );
+}
+
+export default App;
